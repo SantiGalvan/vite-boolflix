9cdb49bdd8e89e1605173c28f81d8e67c40f87ea
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 22:19:59 +0100

    added element

--- src/components/Films/FilmsList.vue
+++ src/components/Films/FilmsList.vue
@@ -1,9 +1,19 @@
 <script>
+import { store } from '../../data/store.js'
 export default {
-    name: 'FilmsList'
+    name: 'FilmsList',
+    data: () => ({ store }),
 }
 </script>
 
-<template></template>
+<template>
+    <section>
+        <div class="container my-5">
+            <h2>Movies</h2>
+            <ul class="row g-4">
+            </ul>
+        </div>
+    </section>
+</template>
 
 <style></style>

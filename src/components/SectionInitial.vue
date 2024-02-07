52416f693c7f9266bd4cf548bb50fd1f75368616
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 23:05:21 +0100

    created & added element

---
+++ src/components/SectionInitial.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    name: 'SectionInitial'
+}
+</script>
+
+<template>
+    <section id="section-initial">
+        <div class="container">
+            <h2 class="text-center">Cerca un film o una serie tv</h2>
+        </div>
+    </section>
+</template>
+
+<style></style>

9f864ada4efec349ea076a229cba25bef0cd77e2
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 21:47:15 +0100

    import store

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script>
-import axios from 'axios'
+import axios from 'axios';
+import { store } from './data/store.js'
 import AppHeader from './components/AppHeader.vue';
 import AppMain from './components/AppMain.vue';
 
 // Variabili API
-const endpointTv = 'https://api.themoviedb.org/3/search/tv'
-const endpointMovie = 'https://api.themoviedb.org/3/search/movie'
-const query = '?query='
-const apiKey = '&api_key=8b15dd81bdce2600f4ff499a5c65a455'
+const endpointTv = 'https://api.themoviedb.org/3/search/tv';
+const endpointMovie = 'https://api.themoviedb.org/3/search/movie';
+const query = '?query=';
+const apiKey = '&api_key=8b15dd81bdce2600f4ff499a5c65a455';
 
 export default {
     name: 'Boolflix',

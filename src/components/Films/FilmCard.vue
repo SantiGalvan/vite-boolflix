a07878aadd32c0dfce6e2ad313d7070c3a621c1d
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 22:52:51 +0100

    added v-if & v-else

--- src/components/Films/FilmCard.vue
+++ src/components/Films/FilmCard.vue
@@ -11,7 +11,10 @@ export default {
     <li v-for="movie in movies" :key="movie.id" class="col-3">
         <h4>{{ movie.name || movie.title }}</h4>
         <p>{{ movie.original_name || movie.original_title }}</p>
-        <p>{{ movie.original_language }}</p>
+        <img v-if="movie.original_language === 'en' || movie.original_language === 'it'"
+            :src="`src/assets/img/${movie.original_language}.png`" :alt="movie.original_language"
+            class="img-fluid img-lang">
+        <p v-else>{{ movie.original_language }}</p>
         <p>{{ movie.vote_average }}</p>
     </li>
 </template>

e8815c7aafdb9fe6da3eb42d29ce1523b5dcdd7e
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 22:35:45 +0100

    import FilmCard

--- src/components/Films/FilmsList.vue
+++ src/components/Films/FilmsList.vue
@@ -1,16 +1,21 @@
 <script>
-import { store } from '../../data/store.js'
+import FilmCard from './FilmCard.vue';
 export default {
     name: 'FilmsList',
-    data: () => ({ store }),
+    components: { FilmCard },
+    props: {
+        tv: Array,
+        sectionTitle: String
+    }
 }
 </script>
 
 <template>
     <section>
         <div class="container my-5">
-            <h2>Movies</h2>
+            <h2>{{ sectionTitle }}</h2>
             <ul class="row g-4">
+                <FilmCard :movies="tv" />
             </ul>
         </div>
     </section>

445c86beeee6d22aadd3255e82dd4de6da37616f
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 22:35:12 +0100

    created FilmCard & added element

---
+++ src/components/Films/FilmCard.vue
@@ -0,0 +1,19 @@
+<script>
+export default {
+    name: 'FilmCard',
+    props: {
+        movies: Array
+    }
+}
+</script>
+
+<template>
+    <li v-for="movie in movies" :key="movie.id" class="col-3">
+        <h4>{{ movie.name || movie.title }}</h4>
+        <p>{{ movie.original_name || movie.original_title }}</p>
+        <p>{{ movie.original_language }}</p>
+        <p>{{ movie.vote_average }}</p>
+    </li>
+</template>
+
+<style></style>

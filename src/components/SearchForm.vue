109258cb39b6aa86b00ba4e6c2e9d31d1b5f1a09
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 19:10:17 +0100

    added emit

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -6,14 +6,15 @@ export default {
     }),
     props: {
         placeholder: String
-    }
+    },
+    emits: ['submit-form']
 }
 </script>
 
 <template>
-    <form>
+    <form @submit.prevent="$emit('submit-form', searchTerm)">
         <div class="input-group">
-            <input type="text" class="form-control" :placeholder="placeholder" v-model="searchTerm">
+            <input type="text" class="form-control" :placeholder="placeholder" v-model.trim="searchTerm">
             <button class="btn" type="button" id="button-addon2">Cerca</button>
         </div>
     </form>

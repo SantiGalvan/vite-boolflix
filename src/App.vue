fafd598fef541bbcc1417bb50a9324f5b4712afb
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-07 18:41:51 +0100

    creataed variables API

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,12 @@
 import axios from 'axios'
 import AppHeader from './components/AppHeader.vue';
 
+// Variabili API
+const endpointTv = 'https://api.themoviedb.org/3/search/tv'
+const endpointMovie = 'https://api.themoviedb.org/3/search/movie'
+const query = '?query='
+const apiKey = '&api_key=8b15dd81bdce2600f4ff499a5c65a455'
+
 export default {
     name: 'Boolflix',
     components: { AppHeader }

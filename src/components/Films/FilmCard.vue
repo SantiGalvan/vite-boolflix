bea04de18f46471caf0237e4f7c56a4ea73fe189
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-08 12:13:13 +0100

    added computed

--- src/components/Films/FilmCard.vue
+++ src/components/Films/FilmCard.vue
@@ -4,16 +4,27 @@ export default {
     props: {
         productions: Object
     },
+    computed: {
+        name() {
+            return this.productions.name || this.productions.title
+        },
+        originalTitle() {
+            return this.productions.original_name || this.productions.original_title
+        },
+        hasFlag() {
+            const flags = ['en', 'it'];
+            return flags.includes(this.productions.original_language);
+        }
+    }
 }
 </script>
 
 <template>
     <li class="col-3">
-        <h4>{{ productions.name || productions.title }}</h4>
-        <p>{{ productions.original_name || productions.original_title }}</p>
-        <img v-if="productions.original_language === 'en' || productions.original_language === 'it'"
-            :src="`src/assets/img/${productions.original_language}.png`" :alt="productions.original_language"
-            class="img-fluid img-lang">
+        <h4>{{ name }}</h4>
+        <p>{{ originalTitle }}</p>
+        <img v-if="hasFlag" :src="`src/assets/img/${productions.original_language}.png`"
+            :alt="productions.original_language" class="img-fluid img-lang">
         <p v-else>{{ productions.original_language }}</p>
         <p>{{ productions.vote_average }}</p>
     </li>

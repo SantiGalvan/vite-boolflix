bd5784574d8cf25744022c65ddfc86c95c0042cb
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-08 19:22:54 +0100

    added element

--- src/components/Films/FilmCard.vue
+++ src/components/Films/FilmCard.vue
@@ -20,27 +20,44 @@ export default {
             return url.href;
         },
         coverSrc() {
-            if (!this.productions.backdrop_path) {
+            if (!this.productions.poster_path) {
                 return 'src/assets/img/not-image.jpg'
             }
-            return `https://image.tmdb.org/t/p/w342/${this.productions.backdrop_path}`
+            return `https://image.tmdb.org/t/p/w342/${this.productions.poster_path}`
         },
         voteAverage() {
             const voteAverage = parseInt(this.productions.vote_average / 2)
             return voteAverage
-        }
+        },
     }
 }
 </script>
 
 <template>
-    <li class="col-3">
-        <h4>{{ name }}</h4>
-        <p>{{ originalTitle }}</p>
-        <img v-if="hasFlag" :src="flagSrc" :alt="productions.original_language" class="img-fluid img-lang">
-        <p v-else>{{ productions.original_language }}</p>
-        <p>{{ voteAverage }}</p>
-        <img :src="coverSrc" :alt="productions.original_language" class="cover-img">
+    <li class="col col-3">
+        <div class="card">
+            <figure>
+                <img :src="coverSrc" :alt="productions.original_language" class="img-fluid cover-img">
+            </figure>
+            <div class="card-body">
+                <h3 class="card-title mb-4">{{ name }}</h3>
+                <h6 class="card-subtitle mb-4">{{ originalTitle }}</h6>
+                <div v-if="hasFlag" class="language-img text-center">
+                    <img :src="flagSrc" :alt="productions.original_language" class="mb-4 img-fluid img-lang">
+                </div>
+                <div v-else class="language-text text-center">
+                    <p>{{ productions.original_language }}</p>
+                </div>
+                <div class="rating text-center">
+                    <p>{{ voteAverage }}</p>
+                    <FontAwesomeIcon icon="fa-solid fa-star" />
+                    <FontAwesomeIcon icon="fa-regular fa-star" />
+                    <FontAwesomeIcon icon="fa-regular fa-star" />
+                    <FontAwesomeIcon icon="fa-regular fa-star" />
+                    <FontAwesomeIcon icon="fa-regular fa-star" />
+                </div>
+            </div>
+        </div>
     </li>
 </template>
 

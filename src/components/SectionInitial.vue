23015730cea6cb686fe5431746af48ac6181c519
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-08 16:33:28 +0100

    try Font Awesome

--- src/components/SectionInitial.vue
+++ src/components/SectionInitial.vue
@@ -8,6 +8,8 @@ export default {
     <section id="section-initial">
         <div class="container">
             <h2 class="text-center">Cerca un film o una serie tv</h2>
+            <FontAwesomeIcon icon="fa-solid fa-star" />
+            <FontAwesomeIcon icon="fa-regular fa-smile" />
         </div>
     </section>
 </template>

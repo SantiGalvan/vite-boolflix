56a97cd7faa96be9c63438af0777b5fb3658af06
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-09 13:07:03 +0100

    added select & prop

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -2,18 +2,24 @@
 export default {
     name: 'SearchForm',
     data: () => ({
-        searchTerm: ''
+        searchTerm: '',
+        selectOption: ''
     }),
     props: {
         placeholder: String,
-        buttonLabel: String
+        buttonLabel: String,
+        genres: Array
     },
     emits: ['submit-form', 'input-change']
 }
 </script>
 
 <template>
-    <form @submit.prevent="$emit('submit-form')">
+    <form @submit.prevent="$emit('submit-form')" class="d-flex gap-2">
+        <select class="form-select" v-model="selectOption">
+            <option value="">All</option>
+            <option v-for="genre in genres" :key="genre.id" :value="genre.id">{{ genre.name }}</option>
+        </select>
         <div class="input-group">
             <input type="text" class="form-control" @keyup="$emit('input-change', searchTerm)" :placeholder="placeholder"
                 v-model.trim="searchTerm">

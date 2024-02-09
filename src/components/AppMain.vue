78a37190024f525fd93266fb02ad4a628b7f6ae6
SantiGalvan <santiagogalvancolorado@gmail.com>
2024-02-09 16:35:57 +0100

    fixed v-if, v-else-if & v-else

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -13,17 +13,21 @@ export default {
     <!-- Main -->
     <main>
 
-        <!-- Section Movies -->
-        <FilmsList v-if="store.films.length !== 0" :movies="store.films" sectionTitle="Movies" />
+        <div v-if="store.films.length !== 0 && store.movies.length !== 0">
+            <!-- Section Movies -->
+            <FilmsList :movies="store.films" sectionTitle="Movies" />
 
-        <!-- Section TV Series -->
-        <FilmsList v-if="store.movies.length !== 0" :movies="store.movies" sectionTitle="TV Series" />
+            <!-- Section TV Series -->
+            <FilmsList :movies="store.movies" sectionTitle="TV Series" />
+        </div>
 
-        <!-- Section Movies Genres -->
-        <FilmsList v-if="store.genreFilms.length !== 0" :movies="store.genreFilms" sectionTitle="Movies" />
+        <div v-else-if="store.genreFilms.length !== 0 && store.genreMovies.length !== 0">
+            <!-- Section Movies Genres -->
+            <FilmsList v-if="store.genreFilms.length !== 0" :movies="store.genreFilms" sectionTitle="Movies" />
 
-        <!-- Section TV Series Genres -->
-        <FilmsList v-if="store.genreMovies.length !== 0" :movies="store.genreMovies" sectionTitle="TV Series" />
+            <!-- Section TV Series Genres -->
+            <FilmsList v-if="store.genreMovies.length !== 0" :movies="store.genreMovies" sectionTitle="TV Series" />
+        </div>
 
         <!-- Section Initial -->
         <SectionInitial v-else />
